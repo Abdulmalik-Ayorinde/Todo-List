609109ec801a5d391fa4c002908fd4742584fc98
Abdulmalik-Ayorinde <malik.ayo4life@gmail.com>
2019-11-18 12:51:49 +0100

    My first commit

---
+++ assets/js/todos.js
@@ -0,0 +1,26 @@
+//adding line through
+$("ul").on("click", "li", function () {
+	$(this).toggleClass("completed")
+});	
+
+// adding the delete function
+$("ul").on("click", "span", function (event) {
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+event.stopPropagation();
+});
+
+//adding new todos from the input 
+$("input[type='text'").keypress(function(event){
+	if (event.which === 13) {
+	var inputContent = $(this).val();
+	$(this).val("");
+	$("ul").append("<li><span><i class='fa fa-trash'>Tr </i></span> " + inputContent + "</li>");
+	}
+});
+
+//Making the plus button work
+$(".fa-plus").click(function(){
+	$("input[type='text'").toggle();
+});
